dd879a680d2fbc4ac4db243435b0f52611a2d780
Gustavo Velazquez <Gustavo.Velazquez@encora.com>
2022-03-21 12:36:25 -0600

    Removed line colors

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -10,12 +10,12 @@ require("indent_blankline").setup {
   -- for example, context is off by default, use this to turn it on
   show_current_context = true,
   show_current_context_start = true,
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-    "IndentBlanklineIndent3",
-    "IndentBlanklineIndent4",
-    "IndentBlanklineIndent5",
-    "IndentBlanklineIndent6",
-  },
+  --char_highlight_list = {
+    --"IndentBlanklineIndent1",
+    --"IndentBlanklineIndent2",
+    --"IndentBlanklineIndent3",
+    --"IndentBlanklineIndent4",
+    --"IndentBlanklineIndent5",
+    --"IndentBlanklineIndent6",
+  --},
 }
